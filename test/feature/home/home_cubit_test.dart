3488df24701ecdf58266d10e63275559fb78fc2b
aorlando <anto.orla@gmail.com>
2024-04-09 22:24:21 +0200

    init

---
+++ test/feature/home/home_cubit_test.dart
@@ -0,0 +1,7 @@
+import 'package:aorlando/feature/home/presentation/blocs/home_cubit.dart';
+import 'package:mockito/annotations.dart';
+
+@GenerateNiceMocks([
+  MockSpec<HomeCubit>(),
+])
+void main() {}
