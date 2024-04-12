7ed5a00b2ac6d075ada14199fc75c2b0b1981021
aorlando <anto.orla@gmail.com>
2024-04-12 10:35:06 +0200

    home_cubit_test works

--- test/feature/home/home_cubit_test.dart
+++ test/feature/home/home_cubit_test.dart
@@ -1,17 +1,11 @@
 import 'package:aorlando/core/utils/errors/failure_entity.dart';
 import 'package:aorlando/feature/home/domain/entities/cv_entity.dart';
 import 'package:aorlando/feature/home/domain/use_cases/get_cv_use_case.dart';
-import 'package:aorlando/core/utils/errors/failure_entity.dart';
-import 'package:aorlando/feature/home/domain/entities/cv_entity.dart';
-import 'package:aorlando/feature/home/domain/use_cases/get_cv_use_case.dart';
 import 'package:aorlando/feature/home/presentation/blocs/home_cubit.dart';
 import 'package:bloc_test/bloc_test.dart';
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
-import 'package:mockito/mockito.dart';
-
-import 'home_cubit_test.mocks.dart';
 import 'package:mockito/mockito.dart';
 
 import 'home_cubit_test.mocks.dart';
